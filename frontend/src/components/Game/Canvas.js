ab0d49be1a430f8809100d3e50870875620562f4
Anuj Gosavi <anujgosavi2005@gmail.com>
2025-07-03 20:20:32 +0530

    fixed the player video  issue

--- frontend/src/components/Game/Canvas.js
+++ frontend/src/components/Game/Canvas.js
@@ -210,8 +210,25 @@ const Canvas = () => {
     };
   }, [interactionMenu, playerName]);
 
-  // WebRTC config (use public STUN for demo)
-  const rtcConfig = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
+  // WebRTC config (use Xirsys STUN/TURN for production)
+  const rtcConfig = {
+    iceServers: [
+      { urls: ["stun:bn-turn1.xirsys.com"] },
+      {
+        username:
+          "lk11OZP5aa3h51oLgGVIOg8q9D623_Rsp8vWjV3GxdjxsprkjyNOlvuJQQRuHx1QAAAAAGhmmBRUdXJuQW51ag==",
+        credential: "b0377942-581c-11f0-8bcc-0242ac140004",
+        urls: [
+          "turn:bn-turn1.xirsys.com:80?transport=udp",
+          "turn:bn-turn1.xirsys.com:3478?transport=udp",
+          "turn:bn-turn1.xirsys.com:80?transport=tcp",
+          "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+          "turns:bn-turn1.xirsys.com:443?transport=tcp",
+          "turns:bn-turn1.xirsys.com:5349?transport=tcp",
+        ],
+      },
+    ],
+  };
 
   // Accept incoming call
   const handleAcceptCall = async () => {
